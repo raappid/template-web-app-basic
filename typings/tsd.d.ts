f892ebe9c4fac69438b235c2f11cf4e8e2ac0a4b
varunreddy <varun8686@gmail.com>
2016-01-31 13:40:57 -0700

    adding typings

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="eventemitter3/eventemitter3.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="bluebird/bluebird.d.ts" />
-/// <reference path="jasmine/jasmine.d.ts" />
