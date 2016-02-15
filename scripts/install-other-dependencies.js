8ab12b0bdaae11947cea5ab95e8e132e0cdefeac
varunreddy <varun8686@gmail.com>
2016-02-14 21:18:30 -0700

    adding shrinkwrap to other dependencies

--- scripts/install-other-dependencies.js
+++ scripts/install-other-dependencies.js
@@ -1,3 +1,14 @@
 
 
-console.log("not implemented");
+var util = require('./util');
+
+util.exec("npm shrinkwrap --dev", function (err) {
+
+    if(err)
+    {
+        console.log(err);
+        process.exit(1);
+    }
+
+   process.exit(0);
+});
