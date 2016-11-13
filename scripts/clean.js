6ba84481b0d802eb6acbb519cef38af1ec82f6c1
varunreddy <varun8686@gmail.com>
2016-11-13 14:15:34 -0700

    testing heroku deploy 7.0

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,19 +1,24 @@
 
 
-var del = require('del');
-var projectConfig = require('../project.config');
+if(process.env.NODE_ENV !== "CI")
+{
+    // cleaning only for local builds as CI will be a clean environment to begin with
+    var del = require('del');
+    var projectConfig = require('../project.config');
 
-var paths = del.sync([
-    'coverage',
-    projectConfig.srcDir +'/**/*.css',
-    '!'+ projectConfig.srcClientDir +'/assets/fonts/**/*.css',
-    projectConfig.srcDir +'/**/*.js',
-    '!'+ projectConfig.srcDir +'/**/libs/**/*',
-    projectConfig.srcDir +'/**/*.map',
-    projectConfig.testDir+"/**/*.js",
-    projectConfig.testDir +'/**/*.map',
-    projectConfig.distDir,'temp',"reporters"]);
+    var paths = del.sync([
+        'coverage',
+        projectConfig.srcDir +'/**/*.css',
+        '!'+ projectConfig.srcClientDir +'/assets/fonts/**/*.css',
+        projectConfig.srcDir +'/**/*.js',
+        '!'+ projectConfig.srcDir +'/**/libs/**/*',
+        projectConfig.srcDir +'/**/*.map',
+        projectConfig.testDir+"/**/*.js",
+        projectConfig.testDir +'/**/*.map',
+        projectConfig.distDir,'temp',"reporters"]);
 
-console.log('Deleted files/folders:\n', paths.join('\n'));
+    console.log('Deleted files/folders:\n', paths.join('\n'));
+}
+
 
 
