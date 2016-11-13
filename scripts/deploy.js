850afeff3aac29321af5f440a881db52e5f416d1
varunreddy <varun8686@gmail.com>
2016-11-13 11:58:50 -0700

    testing firebase deploy 1.0

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,34 @@
 
-console.log("not implemented");
+var util = require('./util');
+
+util.exec("npm run build-release",function(error){
+
+
+    if(error)
+    {
+        console.log(error);
+        process.exit(1)
+    }
+    else
+    {
+        if(process.env.NODE_ENV === "CI")
+        {
+            util.exec("firebase deploy --token $FIREBASE_TOKEN",function(error){
+
+                if(error)
+                {
+                    console.log(error);
+                    process.exit(1)
+                }
+
+                process.exit(0);
+            })
+        }
+        else
+        {
+            process.exit(0);
+        }
+
+    }
+});
 
