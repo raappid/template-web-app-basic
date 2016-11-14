98355b142771816b095958b67d06a0b342ab76f3
varunreddy <varun8686@gmail.com>
2016-11-14 02:44:53 -0700

    fixing coverage reporting to coveralls

--- scripts/report-coverage.js
+++ scripts/report-coverage.js
@@ -1,15 +1,29 @@
 
+var fs = require("fs-extra");
+var path = require("path");
 
-var exec = require('child_process').exec;
+try {
+    // Query the entry
+    fs.lstatSync(path.resolve("./coverage/lcov.info"));
 
-exec("cat ./coverage/lcov.info | ./node_modules/.bin/coveralls", function(err, stdOut, stdErr){
-    console.log(stdOut);
-    console.log(stdErr);
-    if(err)
-    {
-        process.exit(1);
-    }
+    var exec = require('child_process').exec;
 
+    exec("cat ./coverage/lcov.info | ./node_modules/.bin/coveralls", function(err, stdOut, stdErr){
+        console.log(stdOut);
+        console.log(stdErr);
+        if(err)
+        {
+            process.exit(1);
+        }
+
+        process.exit(0);
+    });
+}
+catch (e) {
+
+
+    //means no coverage report was generated
+    console.log("Nothing to Report: Coverage info not found");
     process.exit(0);
-});
 
+}
