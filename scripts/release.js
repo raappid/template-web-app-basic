7327d3e43e6ce29baa9adb9fff3f0eacebd77f9f
coolchem <varun8686@gmail.com>
2017-06-27 13:19:19 -0600

    fixing release process 1.0

--- scripts/release.js
+++ scripts/release.js
@@ -6,37 +6,68 @@ if(process.env.RELEASE_TYPE) //look release build
 
     let cmd = "npm version " + process.env.RELEASE_TYPE;
 
+
+
     util.series([
 
         "npm test",
 
         ["git fetch","fetch all branches"],
 
-        ["git checkout master","checked out master branch"],
-
-        ["git checkout production","checked out production branch"],
-        ["git merge --no-ff --no-edit production master", "merging from Master"],
-
-        [cmd,"increasing version number and tagging"],
-
-        "git push -u --follow-tags",
-
-        ['git checkout master',"checked out master branch.."],
-
-        "git merge --no-ff --no-edit master production",
-
-        "git push",
-
-        ['git branch -D production',"production branch deleted..release Done!!"]
+        ["git checkout master","checked out master branch"]
 
     ],function(err){
+
         if(err)
         {
             console.log(err);
             process.exit(1);
         }
 
-        process.exit(0);
+        util.exec("git checkout production",function(err) {
+
+            if(err)
+            {
+                util.exec("git checkout -b production",function(err) {
+
+                    if(err)
+                    {
+                        console.log(err);
+                        process.exit(1);
+                    }
+
+                    util.series([
+
+                        ["git merge --no-ff --no-edit production master", "merging from Master"],
+
+                        [cmd,"increasing version number and tagging"],
+
+                        "git push -u --follow-tags",
+
+                        ['git checkout master',"checked out master branch.."],
+
+                        "git merge --no-ff --no-edit master production",
+
+                        "git push",
+
+                        ['git branch -D production',"production branch deleted..release Done!!"]
+
+                    ],function(err){
+                        if(err)
+                        {
+                            console.log(err);
+                            process.exit(1);
+                        }
+
+                        process.exit(0);
+                    });
+
+
+                });
+            }
+
+        });
+
     });
 
 }
